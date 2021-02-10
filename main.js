d05a82802726f85d59ddfedb3d2bfe3b0c61d94e
youjinDev <67622600+youjinDev@users.noreply.github.com>
2021-02-10 13:39:09 +0900

    JavaScript : scroll to section

--- main.js
+++ main.js
@@ -11,3 +11,19 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link==null) {
+        return;
+    }
+    console.log(link);
+
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+
+});
