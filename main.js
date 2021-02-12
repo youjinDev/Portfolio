11f5bcea05180cb231597a3dbe6e768309120ddc
youjinDev <67622600+youjinDev@users.noreply.github.com>
2021-02-12 20:34:25 +0900

    JavaScript : home opacity

--- main.js
+++ main.js
@@ -30,7 +30,14 @@ contactMe.addEventListener('click', (event) => {
     scrollIntoView('#contact')
 });
 
-function scrollIntoView(selector) {
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior: "smooth"});
-}
+// transparent home section when it scrolled
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+   home.style.opacity = 1 - window.scrollY/homeHeight;
+    });
+
+    function scrollIntoView(selector) {
+        const scrollTo = document.querySelector(selector);
+        scrollTo.scrollIntoView({behavior: "smooth"});
+    }
